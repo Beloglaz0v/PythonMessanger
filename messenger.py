8de75f53ea37aac152935bd41ff6735b8b448225
Beloglaz0v <beloglazov-daniil@mail.ru>
2020-05-12 13:17:43 +0300

    v2.0

--- messenger.py
+++ messenger.py
@@ -1,16 +1,18 @@
 import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import auth_form  # Это наш конвертированный файл дизайна
 import requests
 import main_form
+import dialog
+import time
 
 
 class MessengerApp(QtWidgets.QMainWindow, auth_form.Ui_form_auth):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        #self.btn_auth.pressed.connect(self.authorization)
-        self.btn_auth.pressed.connect(self.testbut)
+        self.btn_auth.pressed.connect(self.authorization)
+        #self.btn_auth.pressed.connect(self.testbut)
         self.server = 'http://127.0.0.1:5000'
 
 
@@ -44,41 +46,165 @@ class MessengerMainApp(QtWidgets.QMainWindow, main_form.Ui_MainWindow):
         self.setupUi(self)  # Инициализация дизайна
         self.server = 'http://127.0.0.1:5000'
         self.id = user_id
-        self.employees = self.list_of_employees()
+        self.personal_info = {}
+        self.employees = []
+        self.list_of_employees()
         self.filling_1tab()
         self.filling_2tab()
+        self.filling_3tab()
+        self.tabWidget.currentChanged.connect(self.filling_3tab)
+        self.list_dialogs.doubleClicked.connect(self.open_chat)
+        self.tabWidget.setCurrentIndex(0)
         self.listWidget.doubleClicked.connect(self.addit_info)
+        self.dialog_list = []
+        self.dialog_id = -1
+        self.count_unread = 0
+        self.last_message_time = 0
+        self.btn_send_message.pressed.connect(self.send_message)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.getUpdates)
+        self.timer.start(1000)
+
+
+    def getUpdates(self):
+        if self.tabWidget.currentIndex() == 3 and self.dialog_id != -1:
+            response = requests.post(self.server + "/new_messages", json={'dialog_id': self.dialog_id, 'time': self.last_message_time})
+            data = response.json()['messages']
+            print(data)
+            if data:
+                    for el in data:
+                        if int(el['dialog_id']) == self.dialog_id:
+                            if el['user_id'] == self.id:
+                                line = f"{time.ctime(float(el['time']))}\n{el['message']} "
+                                gg = QtWidgets.QListWidgetItem(line)
+                                gg.setTextAlignment(2)
+                                self.listWidget_chat.addItem(gg)
+                            else:
+                                line = f"{time.ctime(float(el['time']))}\n{el['message']}"
+                                QtWidgets.QListWidgetItem(line, self.listWidget_chat)
+                        else:
+                            self.count_unread += 1
+                            self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_dialogs), _translate("MainWindow",
+                                                                                             "Диалоги"+ str(self.count_unread)))
+                    self.setLast_message_time(data[-1]['time'])
+
+    def setLast_message_time(self, time):
+        self.last_message_time = float(time)
+
+    def setdiaologs_list(self, list):
+        self.dialog_list = list
+
+    def setDialogid(self, id):
+        self.dialog_id = int(id)
 
     def addit_info(self):
-        print(self.employees)
-        print(self.listWidget.currentRow())
-        print(self.employees[self.listWidget.currentRow()])
+        dia = DialogInfo(self, self.employees[self.listWidget.currentRow()])
+        dia.show()
+        dia.exec_()
+
+
+    def open_chat(self):
+        dia = self.dialog_list[self.list_dialogs.currentRow()]
+        dia['dialog_id']
 
     def list_of_employees(self):
         response = requests.get(self.server + "/getemployees")
-        data = response.json()
-        return data['info']
+        data = response.json()['employees']
+        for element in data:
+            if element['id'] == self.id:
+                self.personal_info = element
+            else:
+                self.employees.append(element)
 
     def filling_1tab(self):
-        for element in self.employees:
-            if element['id'] == self.id:
-                data = element
-                break
-        self.lbl_phone.setText(data['phone_num'])
-        self.lbl_email.setText(data['email'])
-        self.lbl_depnum.setText(str(data['dep_num']))
-        self.lbl_surname.setText(data['surname'])
-        self.lbl_name.setText(data['name'])
-        self.lbl_patr.setText(data['patronymic'])
-        self.lbl_birthday.setText(data['birthday'])
+        self.lbl_phone.setText(self.personal_info['phone_num'])
+        self.lbl_email.setText(self.personal_info['email'])
+        self.lbl_depnum.setText(str(self.personal_info['dep_num']))
+        self.lbl_surname.setText(self.personal_info['surname'])
+        self.lbl_name.setText(self.personal_info['name'])
+        self.lbl_patr.setText(self.personal_info['patronymic'])
+        self.lbl_birthday.setText(self.personal_info['birthday'])
 
     def filling_2tab(self):
-        response = requests.get(self.server + "/getallinfo")
-        data = response.json()
-        self.employees = data['info']
-        for el in self.employees:
-            line = f'{el["surname"]} {el["name"]} {el["patronymic"]} {el["phone_num"]}'
+        data = self.employees
+        for el in data:
+            dep = str(el["dep_num"]) if len(str(el["dep_num"])) == 2 else str(el["dep_num"])+'  '
+            line = f'Одел №{dep} {el["surname"]} {el["name"]} {el["patronymic"]}'
             self.listWidget.addItem(line)
+
+    def filling_3tab(self):
+        self.list_dialogs.clear()
+        response = requests.post(self.server + '/get_dialogs', json={'user': self.id})
+        dialogs = response.json()['dialogs']
+        self.setdiaologs_list(dialogs)
+        if dialogs:
+            for dialog in dialogs:
+                line = f"{dialog['name']} {dialog['surname']} \n {time.ctime(float(dialog['time']))} {dialog['message']}"
+                self.list_dialogs.addItem(line)
+
+    def send_message(self):
+        message = self.txt_message.toPlainText()
+        self.txt_message.clear()
+        self.txt_message.repaint()
+        if message:
+            response = requests.post(self.server+'/send', json={'user': self.id,
+                                                                'dialog_id': self.dialog_id,
+                                                                'message': message,
+                                                                })
+            data = response.json()
+            if not data['ok']:
+                line = f"Ошибка отправки сообщения\n{message}"
+                gg = QtWidgets.QListWidgetItem(line)
+                gg.setTextAlignment(2)
+                self.listWidget_chat.addItem(gg)
+
+
+class DialogInfo(QtWidgets.QDialog, dialog.Ui_Dialog):
+    def __init__(self, main, employee):
+        super().__init__()
+        self.main = main
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.employee = employee
+        self.fio = f"{employee['surname']} {employee['name']} {employee['patronymic']}"
+        self.lbl_FIO.setText(self.fio)
+        self.lbl_birthday.setText(employee['birthday'])
+        self.lbl_email.setText(employee['email'])
+        self.lbl_phone.setText(employee['phone_num'])
+        self.lbl_dep_num.setText(str(employee['dep_num']))
+        self.btn_close.pressed.connect(self.close)
+        self.btn_write.pressed.connect(self.openchat)
+
+    def openchat(self):
+        self.main.tabWidget.setCurrentIndex(3)
+        self.main.lbl_FIO_chat.setText(self.fio)
+        self.main.listWidget_chat.clear()
+        if self.main.id < self.employee['id']:
+            user1 = self.main.id
+            user2 = self.employee['id']
+        else:
+            user1 = self.employee['id']
+            user2 = self.main.id
+        print({'user1': user1,'user2': user2})
+        response = requests.post(self.main.server + "/history",
+                                 json={'user1': user1,
+                                       'user2': user2
+                                       })
+        data = response.json()
+        print(data)
+        self.main.setDialogid(data['dialog_id'])
+        history = data['messages']
+        if history:
+            for el in history:
+                if el['user_id'] == self.main.id:
+                    line = f"{time.ctime(float(el['time']))}\n{el['message']} "
+                    gg = QtWidgets.QListWidgetItem(line)
+                    gg.setTextAlignment(2)
+                    self.main.listWidget_chat.addItem(gg)
+                else:
+                    line = f"{time.ctime(float(el['time']))}\n{el['message']}"
+                    QtWidgets.QListWidgetItem(line, self.main.listWidget_chat)
+            self.main.setLast_message_time(history[-1]['time'])
+        self.close()
 
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
--- server.py
+++ server.py
@@ -5,16 +5,6 @@ import sqlite3
 
 app = Flask(__name__)
 
-messages = [
-    {"username": "Jack", "text": "Hi", 'time': time.time()},
-    {"username": "Polly", "text": "Kokoko", 'time': time.time()},
-]
-users = [
-    {"id": 1, "login": 123, "password": 'qwe'},
-    {"id": 2, "login": 1234, "password": 'qweq'},
-    {"id": 3, "login": 1235, "password": 'qwee'},
-]
-
 
 @app.route("/auth", methods=['POST'])
 def auth():
@@ -27,9 +17,7 @@ def auth():
     login = data['login']
     password = data['password']
     for user in users:
-        print(user)
         answer = {"id": False, "login": True, "password": True}
-        print(login, password)
         if user[1] != login:
             answer["login"] = False
         elif user[2] != password:
@@ -42,24 +30,24 @@ def auth():
     return answer
 
 
-@app.route("/getinfo", methods=['POST'])
-def personalinfo():
-    id = request.json['id']
-    conn = sqlite3.connect("messanger.db")
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE id = {int(id)}")
-    info = cur.fetchall()[0]
-    conn.commit()
-    conn.close()
-    answer = {'phone_num': info[3],
-              'email': info[4],
-              'dep_num': info[5],
-              'surname': info[6],
-              'name': info[7],
-              'patronymic': info[8],
-              'birthday': info[9],
-              }
-    return answer
+# @app.route("/getinfo", methods=['POST'])
+# def personalinfo():
+#     id = request.json['id']
+#     conn = sqlite3.connect("messanger.db")
+#     cur = conn.cursor()
+#     cur.execute(f"SELECT * FROM users WHERE id = {int(id)}")
+#     info = cur.fetchall()[0]
+#     conn.commit()
+#     conn.close()
+#     answer = {'phone_num': info[3],
+#               'email': info[4],
+#               'dep_num': info[5],
+#               'surname': info[6],
+#               'name': info[7],
+#               'patronymic': info[8],
+#               'birthday': info[9],
+#               }
+#     return answer
 
 
 @app.route("/getemployees")
@@ -67,7 +55,7 @@ def allpersonalinfo():
     answer = []
     conn = sqlite3.connect("messanger.db")
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users")
+    cur.execute(f"SELECT * FROM users ORDER BY dep_num")
     info = cur.fetchall()
     conn.commit()
     conn.close()
@@ -81,28 +69,119 @@ def allpersonalinfo():
                        'birthday': el[9],
                        'id': el[0],
                        })
-    print(answer)
     return {'employees': answer}
 
 
 @app.route("/send", methods=['POST'])
 def send():
-    """
-    request: {"username": "str", "text": "str"}
-    response: {"ok": true}
-    """
+
     data = request.json
-    username = data['username']
-    text = data['text']
+    print(data)
+    user_id = data['user']
+    text = data['message']
+    dialog_id = data['dialog_id']
+    print(data)
+    try:
+        conn = sqlite3.connect("messanger.db")
+        cur = conn.cursor()
+        print('norm')
+        cur.execute(f'INSERT INTO "main"."messages" ("dialog_id", "user_id", "message", "timedate")'
+                    f' VALUES({dialog_id}, {user_id}, \'{text}\', \'{time.time()}\')')
 
-    messages.append({'username': username, 'text': text, 'date': time.time()})
+        print('norm')
+        conn.commit()
+        conn.close()
+        print('norm')
+        return {'ok': True}
 
-    return {'ok': True}
+    except Exception as ex:
+        print(ex)
+        return {'ok': False}
 
 
-@app.route("/history")
+@app.route("/history", methods=['POST'])
 def history():
-    return {'messages': messages}
+    data = request.json
+    print(data)
+    answer = []
+    conn = sqlite3.connect("messanger.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT id FROM dialogs WHERE user_id = {data['user1']} and user2_id = {data['user2']}")
+    dialog = cur.fetchall()
+    if dialog:
+        cur.execute(f"SELECT * FROM messages WHERE dialog_id = {dialog[0][0]}")
+        history = cur.fetchall()
+        if history:
+            for messange in history:
+                mess = {
+                    "user_id": messange[1],
+                    "message": messange[2],
+                    "attachment": messange[3],
+                    "time": messange[4],
+                }
+                answer.append(mess)
+    else:
+        cur.execute(f'INSERT INTO "main"."dialogs" ("user_id", "user2_id") VALUES({data["user1"]}, {data["user2"]})')
+        cur.execute(f"SELECT id FROM dialogs WHERE user_id = {data['user1']} and user2_id = {data['user2']}")
+        dialog = cur.fetchall()
+        answer = {}
+    conn.commit()
+    conn.close()
+    return {'messages': answer, 'dialog_id': dialog[0][0]}
 
+
+@app.route("/new_messages", methods=['POST'])
+def new_messages():
+    data = request.json
+    print(data)
+    dialog_id = data['dialog_id']
+    last_message_time = data['time']
+    answer = []
+    conn = sqlite3.connect("messanger.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM messages WHERE dialog_id = {dialog_id} and timedate>{last_message_time}")
+    messages = cur.fetchall()
+    if messages:
+        for message in messages:
+            mess = {
+                    "dialog_id": message[0],
+                    "user_id": message[1],
+                    "message": message[2],
+                    "attachment": message[3],
+                    "time": message[4],
+            }
+            answer.append(mess)
+    return {'messages': answer}
+
+
+@app.route("/get_dialogs", methods=['POST'])
+def get_dialogs():
+    data = request.json
+    user = data['user']
+    answer = []
+    conn = sqlite3.connect("messanger.db")
+    cur = conn.cursor()
+    cur.execute(f"""Select messages.dialog_id, messages.message, x.name, x.surname, max(timedate) from messages join
+    (SELECT users.surname as surname, users.name as name, n.d_id as dialog_id FROM USERS JOIN
+    (SELECT user_id as id, id as d_id FROM dialogs WHERE user2_id ={user}) as n
+    on users.id = n.id
+    union
+    Select users.surname as surname, users.name, b.d_id as dialog_id FROM users join
+    (SELECT user2_id as id, id as d_id FROM dialogs WHERE user_id ={user}) as b
+    on users.id = b.id )as x
+    on messages.dialog_id = x.dialog_id 
+    group by messages.dialog_id""")
+    dialogs = cur.fetchall()
+    if dialogs:
+        for dialog in dialogs:
+            dia = {
+                    "dialog_id": dialog[0],
+                    "message": dialog[1],
+                    "name": dialog[2],
+                    "surname": dialog[3],
+                    "time": dialog[4],
+            }
+            answer.append(dia)
+    return {'dialogs': answer}
 
 app.run()
