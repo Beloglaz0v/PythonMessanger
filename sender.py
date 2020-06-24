0366288f4a4574bfd5f3a254f5de79bbaf971775
Beloglazov Daniil <39886690+Beloglaz0v@users.noreply.github.com>
2020-06-24 18:50:56 +0300

    Delete sender.py
    Удаление мусора

--- sender.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-with open('widget_icon.png', 'rb') as f:
-    file = f.read()
-response = requests.get("http://127.0.0.1:5000/get_image")
-with open('icon.png', 'wb')as f:
-    f.write(response.content)
-print(response.content)
